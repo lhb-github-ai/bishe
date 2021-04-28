262caaaf8f90ca737ac1e9e38d9ab8120c292ff6
Rascal <1799044809@qq.com>
2021-04-28 21:52:26 +0800

    阿里云密钥更换

--- exam_parent/service/service_exam/src/main/java/com/lee/exam/service/impl/AvatarServiceImpl.java
+++ exam_parent/service/service_exam/src/main/java/com/lee/exam/service/impl/AvatarServiceImpl.java
@@ -18,11 +18,11 @@ public class AvatarServiceImpl implements AvatarService {
     @Override
     public String uploadFileAvatar(MultipartFile file) {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        String endpoint = "";
 // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4GKHFLd4erXnxoKVQmkn";
-        String accessKeySecret = "S5jzXIywYuHIC4rVjcpM4jDRbHszDB";
-        String bucketName = "studylee";
+        String accessKeyId = "";
+        String accessKeySecret = "";
+        String bucketName = "";
         try (InputStream inputStream = file.getInputStream()) {
             // 创建OSSClient实例。
             OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
